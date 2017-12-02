423b62395f574a2a2a2570edd19c1d60e0b9a0db
Canijo56 <placido.fdez.viagas@gmail.com>
2017-12-02 17:19:25 +0100

    First Major Changes: Stores...

---
+++ Assets/Scripts/StoreController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreController : MonoBehaviour {
+
+    public int storeID;
+    private int currentDiscount = 0;
+    // Setters
+    public void setDiscount(int newDiscount)
+    {
+        switch (newDiscount)
+        {
+            case 0:
+                currentDiscount = 30;
+                break;
+            case 1:
+                currentDiscount = 50;
+                break;
+            case 2:
+                currentDiscount = 70;
+                break;
+            default:
+                currentDiscount = 0;
+                break;
+        }
+    }
+
+    // Getters
+    public int getDiscount()
+    {
+        return currentDiscount;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/StoreManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class Constants
+{
+    public const int numActiveDiscounts = 3; // km per sec.
+    public const float timeOut = 15; // in seconds
+}
+
+public class StoreManager: MonoBehaviour {
+
+  
+
+    GameObject[] storeArray;
+    int[] storesWithDiscount = new int[Constants.numActiveDiscounts];   // 0 - discount = 30%
+                                                                        // 1 - discount = 50%
+                                                                        // 2 - discount = 70%
+
+    float[] storesDiscountTimeOut = new float[Constants.numActiveDiscounts];
+    int storesNumber;
+
+    // Use this for initialization
+    void Start() {
+        storeArray = GameObject.FindGameObjectsWithTag("Store");
+        storesNumber = storeArray.Length;
+        for (int i=0; i < Constants.numActiveDiscounts; ++i)
+        {
+            // 1 second = 50 calls
+            storesDiscountTimeOut[i] = Constants.timeOut + Random.Range(-2, 2);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        assignDiscount();
+        Debug.Log(storesDiscountTimeOut[0]);
+
+	}
+
+    void FixeUpdate()  // called very 20ms
+    {
+  
+    }
+    private void assignDiscount()
+    {
+        for (int i = 0; i < Constants.numActiveDiscounts; ++i)
+        {
+            if (storesDiscountTimeOut[i] <= 0.0)
+            {
+                int newDiscountedStore = Random.Range(0, storesNumber);
+                storesWithDiscount[i] = newDiscountedStore;
+                Debug.Log("TimeOut in store " + i + ". NewDiscount in store " + newDiscountedStore);
+                storeArray[newDiscountedStore].GetComponent<StoreController>().setDiscount(i);
+                storesDiscountTimeOut[i] = Constants.timeOut + Random.Range(-2, 2);
+            }
+
+            storesDiscountTimeOut[i]-= Time.deltaTime;
+        }
+    }
+}
